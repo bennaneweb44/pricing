7326da8fc7918ace0c8fd4e1a2d0cf95c0af2148
Aziz <bennaneweb44@gmail.com>
2020-09-06 00:41:12 +0200

    Security - in progress

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setEmail('nicolasc@sellermania.com');
+        
+        $user->setRoles([
+            'ROLE_USER'
+        ]);
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'sellermania'
+        ));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
